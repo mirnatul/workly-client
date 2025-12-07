2afb722a20917d47133464b1f7a62da2138d9bf3
mirnatul <www.mirnatul@gmail.com>
2025-12-07 12:40:55 +0600

    initial set up and firebase env variable

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.apiKey,
+    authDomain: import.meta.authDomain,
+    projectId: import.meta.projectId,
+    storageBucket: import.meta.storageBucket,
+    messagingSenderId: import.meta.messagingSenderId,
+    appId: import.meta.appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
