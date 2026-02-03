525773a9df830bd9c335609dd5c6d034a84ce9a7
mirnatul <mdmirnatul@gmail.com>
2026-02-03 21:39:26 +0600

    token generated from jwt

--- src/api/applicationsAPI.js
+++ src/api/applicationsAPI.js
@@ -1,4 +1,7 @@
 export const myApplicationsPromise = email => {
     console.log("hit there");
-    return fetch(`http://localhost:3000/applications?email=${email}`).then(res => res.json());
+    return fetch(`http://localhost:3000/applications?email=${email}`, {
+        credentials: 'include'  // include cookies in the request
+    })
+        .then(res => res.json());
 };
--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
+import axios from 'axios';
 
 
 const googleProvider = new GoogleAuthProvider();
@@ -36,7 +37,33 @@ const AuthProvider = ({ children }) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             setLoading(false);
-            // console.log('user in the auth state changed', currentUser);
+
+            // jwt token setting
+            if (currentUser?.email) {
+                const userData = { email: currentUser.email };
+
+                axios.post('http://localhost:3000/jwt', userData, {
+                    withCredentials: true  // in case you want to send cookie with request, set this to true
+                })
+                    .then(res => {
+                        console.log(res.data);
+                    })
+                    .catch(err => {
+                        console.log(err.message);
+                    });
+
+                // axios.post('http://localhost:3000/jwt', userData)
+                //     .then(res => {
+                //         console.log('token after jwt', res.data);
+
+                //         // if you want to store token in local storage
+                //         const token = res.data.token;
+                //         localStorage.setItem('token', token);
+                //     })
+                //     .catch(err => {
+                //         console.log(err.message);
+                //     });
+            }
         });
         return () => unSubscribe();
     }, []);
